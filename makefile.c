2c173bd71fcd386b1b0ae63d6049ccfcc82564a0
HassanTHatGuy <hass.osman12@gmail.com>
2025-09-12 00:26:49 -0500

    makefile.c

---
+++ makefile.c
@@ -0,0 +1,24 @@
+# Makefile for building fibdispatch and creating symlinks
+
+CC = gcc
+CFLAGS = -Wall -O2
+TARGET = fibdispatch
+LINKS = fib rfib ifib
+
+.PHONY: all clean
+
+all: $(TARGET) $(LINKS)
+
+$(TARGET): fibdispatch.c
+        $(CC) $(CFLAGS) -o $(TARGET) fibdispatch.c
+
+$(LINKS): $(TARGET)
+        @for link in $(LINKS); do \
+                if [ -e $$link ] || [ -L $$link ]; then \
+                        rm -f $$link; \
+                fi; \
+                ln -s $(TARGET) $$link; \
+        done
+
+clean:
+        rm -f $(TARGET) $(LINKS)
